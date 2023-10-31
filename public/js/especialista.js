2998d69baebea31498de5834a8b0c4a8395c6cb2
Diego Santana <81384046+Prody-xd@users.noreply.github.com>
2023-10-30 23:38:58 -0300

    search box funcional y cards dinamicas

--- public/js/especialista.js
+++ public/js/especialista.js
@@ -3,35 +3,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('search-button');
     const cardsContainer = document.getElementById('cards-container');
   
+    let pacientesData = [];
     // Hacer la llamada a la API para obtener la lista de pacientes
     fetch('http://localhost:303/api/especialistas')
-      .then((response) => response.json())
-      .then((data) => {
-        // Recorre la lista de pacientes y crea tarjetas para cada uno
-        data.forEach((paciente) => {
-          const card = createCard(paciente);
-          cardsContainer.appendChild(card);
-        });
-      })
-      .catch((error) => {
-        console.error('Error en la solicitud a la API:', error);
-      });
+    .then((response) => response.json())
+    .then((data) => {
+      especialistasData = data; // Almacena los datos de los especialistas
+      renderCards(data); // Renderiza las tarjetas iniciales
+    })
+    .catch((error) => {
+      console.error('Error en la solicitud a la API:', error);
+    });
   
     // Función para crear una tarjeta a partir de un objeto de paciente
     function createCard(paciente) {
       const card = document.createElement('div');
-      card.className = 'card';
+      card.className = 'card-dinamic';
   
       card.innerHTML = `
-        <img src="/images/imgespecialista${paciente.id_especialista}.jpg" alt="Imagen de ${paciente.nombre}">
+        <img src="/images/img${paciente.rut_especialista}.jpg" alt="Imagen de ${paciente.nombre}">
         <div class="card-info">
           <div class="title-buttons">
             <h3>${paciente.nombre_especialista}</h3>
             <div class="buttons">
               <button id="perfil">Ver Perfil</button>
               <button id="agendar">Agendar hora</button>
-            </div>
           </div>
+        </div>
           <p>Especialidad: ${paciente.especialidad}</p>
           <p>Años de experiencia: ${paciente.experiencia}</p>
           <p>Centro Médico: ${paciente.centro_medico}</p>
@@ -40,21 +38,37 @@ document.addEventListener('DOMContentLoaded', () => {
   
       return card;
     }
+    function renderCards(data) {
+      cardsContainer.innerHTML = ''; // Borra las tarjetas existentes
   
-    // Manejar la búsqueda
-    searchButton.addEventListener('click', () => {
+      data.forEach((especialista) => {
+        const card = createCard(especialista);
+        cardsContainer.appendChild(card);
+      });
+    }
+    searchInput.addEventListener('input', () => {
       const query = searchInput.value.toLowerCase();
-      const cards = cardsContainer.querySelectorAll('.card');
   
       // Filtrar las tarjetas según la búsqueda
-      cards.forEach((card) => {
-        const cardNombre = card.querySelector('h3').textContent.toLowerCase();
-        if (cardNombre.includes(query)) {
-          card.style.display = 'block';
-        } else {
-          card.style.display = 'none';
-        }
+      const filteredData = especialistasData.filter((especialista) => {
+        const especialistaData = `${especialista.nombre_especialista} ${especialista.especialidad} ${especialista.experiencia}`.toLowerCase();
+        return especialistaData.includes(query);
       });
+  
+      renderCards(filteredData); // Renderiza las tarjetas filtradas
     });
+    // Manejar la búsqueda
+  searchButton.addEventListener('click', () => {
+    const query = searchInput.value.toLowerCase();
+
+    // Filtrar las tarjetas según la búsqueda
+    const filteredData = especialistasData.filter((especialista) => {
+      const especialistaData = `${especialista.nombre_especialista} ${especialista.especialidad} ${especialista.experiencia}`.toLowerCase();
+      return especialistaData.includes(query);
+    });
+    console.log(query);
+
+    renderCards(filteredData); // Renderiza las tarjetas filtradas
   });
+});
   
