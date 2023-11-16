ce7e43cb56a8f8757c1fa3dd36033ebfacdcf083
DemonWindstrike <edua.nunezc@duocuc.cl>
2023-11-15 22:36:44 -0300

    avance reservahora

--- public/js/horas.js
+++ public/js/horas.js
@@ -1,7 +1,34 @@
-// Aquí puedes escribir JavaScript para manejar eventos, como hacer clic en los botones de reserva
-
-document.getElementById('back-button').addEventListener('click', function() {
-    window.history.back(); // Esto llevará al usuario a la página anterior
-});
-
-// Agrega la lógica para manejar la selección de fechas y horas
+document.addEventListener('DOMContentLoaded', () => {
+    const currentDateElement = document.getElementById('current-date');
+    const calendarElement = document.getElementById('calendar');
+    let selectedDate = new Date();
+  
+    function generateCalendar(date) {
+      calendarElement.innerHTML = ''; // Limpiar el calendario antes de regenerar
+      // Lógica para generar el calendario...
+      // Puedes utilizar una biblioteca como date-fns para facilitar el manejo de fechas
+    }
+  
+    // Agregar interactividad a los botones de horarios
+    document.querySelectorAll('.time-slot').forEach(button => {
+      button.addEventListener('click', () => {
+        // Lógica para seleccionar un horario
+      });
+    });
+  
+    // Agregar interactividad a los botones de navegación de fecha
+    document.getElementById('prev-date').addEventListener('click', () => {
+      selectedDate.setDate(selectedDate.getDate() - 1);
+      currentDateElement.textContent = selectedDate.toLocaleDateString('es', { weekday: 'long', day: 'numeric', month: 'long' });
+      generateCalendar(selectedDate);
+    });
+  
+    document.getElementById('next-date').addEventListener('click', () => {
+      selectedDate.setDate(selectedDate.getDate() + 1);
+      currentDateElement.textContent = selectedDate.toLocaleDateString('es', { weekday: 'long', day: 'numeric', month: 'long' });
+      generateCalendar(selectedDate);
+    });
+  
+    generateCalendar(selectedDate); // Generar el calendario inicialmente
+  });
+  
